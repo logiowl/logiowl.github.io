d0caf82f8adfd28601aea6fe8f96177ae4f68a68
Logi Owl Content Creators Canada inc <57609440+logiowl@users.noreply.github.com>
2022-08-06 11:11:12 -0600

    Add files via upload

---
+++ hotSpots.js
@@ -0,0 +1,18 @@
+var theContainer=document.getElementById("#hotSpotContainer"),
+    theHotSpot=document.getElementById("#hotSpot");
+    alert("hi");
+    fetch("./data.json")
+    .then(response=>response.json())
+    .then(data=>{
+        var theData=data.hotspots;
+        for(let i=0;i<theData.length;i++){
+            theHotSpot.style.left=theData[i].x;
+            theHotSpot.style.top=theData[i].y;
+            theHotSpot.innerText+=theData[i].text;
+            theContainer.appendChild(theHotSpot);
+        }
+    });
+    
+        
+    
+    
