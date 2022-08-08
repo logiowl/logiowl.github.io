1e3f0f456bfb9c9d53cdf6ee3cb301f390d776d3
Logi Owl Content Creators Canada inc <57609440+logiowl@users.noreply.github.com>
2022-08-08 15:21:07 -0600

    c5

--- hotSpots.js
+++ hotSpots.js
@@ -1,13 +1,25 @@
 
      var theContainer=document.getElementById("hotSpotContainer"),
      theHotSpot=document.getElementById("hotSpotComponent");
+    var handleClick=(event)=>{
+
+    alert("hi");
+    
+     let sibling =event.target.parentNode.firstChild;
+     console.log(sibling)
+     while(sibling){
+        sibling.show();
+        sibling=sibling.nextSibling;
+     }
      
+     }
+    
     data={
         "hotspots":[
             {"x":'300px',"y":'50px',"link":"","text":"hello this is the text1", "direction":"up-left","backgroundColor":"#00aabb"},
-            {"x":"500px","y":'300px',"link":"","text":"hello this is the text2",  "direction":"up-right"},
-            {"x":"300px","y":'200px',"link":"","text":"hello this is the text3",  "direction":"down-right"},
-            {"x":"700px","y":'200px',"link":"","text":"hello this is the text4",  "direction":"down-left"}
+            {"x":"500px","y":'300px',"link":"","text":"hello this is the text2",  "direction":"up-right","backgroundColor":"#ffffff"},
+            {"x":"300px","y":'200px',"link":"","text":"hello this is the text3",  "direction":"down-right","backgroundColor":"#ffff00"},
+            {"x":"700px","y":'200px',"link":"","text":"hello this is the text4",  "direction":"down-left","backgroundColor":"#00aabb"}
         ]
     };
     var theData=data.hotspots;
@@ -21,6 +33,9 @@
             theClonedNode.style.top=theData[i].y;
             
             let clonedNodeText=theClonedNode.querySelector("#hotSpotText");
+            clonedNodeText.style.backgroundColor=theData[i].backgroundColor;
+            let clonedNodeHotSpot=theClonedNode.querySelector(".hotSpot");
+            clonedNodeHotSpot.addEventListener("click",handleClick)
             let clonedNodeLine=theClonedNode.querySelector(".hotSpotLine");
             clonedNodeLine.style.zIndex=i;
             let clonedNodePath=theClonedNode.querySelector("path");
@@ -31,8 +46,6 @@
                     clonedNodeCircle.setAttribute("cx",50);
                     clonedNodeText.style.left="-205px"
                     clonedNodeLine.style.left="4px";
-
-
                 break;
                 case "down-right":
                     clonedNodePath.setAttribute("d","M53 55 L60 70 L78 70");
