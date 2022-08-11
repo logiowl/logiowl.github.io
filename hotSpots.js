61bc9ab62c7fecdc8f642d8ab43c94098f1ead9a
Logi Owl Content Creators Canada inc <57609440+logiowl@users.noreply.github.com>
2022-08-11 12:58:48 -0600

    commiting

--- hotSpots.js
+++ hotSpots.js
@@ -1,93 +1,64 @@
 
-    var theContainer=document.getElementById("hotSpotContainer"),
-     theHotSpot=document.getElementById("hotSpot");
-     theHotSpotLabel=document.getElementById("hotSpotComponent");
+    var theContainer=document.getElementById("hotspotContainer"),
+     thehotspot=document.getElementById("hotspot");
+     thehotspotComponent=document.getElementById("hotspotComponent");
     var handleClick=(event)=>{
 
     
         i=parseInt (event.target.getAttribute("number"));
-        let hotSpotText=theHotSpotLabel.querySelector("#hotSpotText");
-        hotSpotText.style.backgroundColor=theData[i].backgroundColor; 
-        hotSpotText.innerHTML=theData[i].text;
-        hotSpotText.style.color=theData[i].textColor;
-        let labelOutlines=theHotSpotLabel.querySelector(".hotSpotLine");
-        
-        let outlinesPath=theHotSpotLabel.querySelector("path");
-        let outlinesCircle=theHotSpotLabel.querySelector("circle");
-        outlinesCircle.setAttribute("fill",theData[i].backgroundColor)
+        let hotspotTextTitle=thehotspotComponent.querySelector("#hotspotTextTitle");
+        let hotspotTextBody=thehotspotComponent.querySelector("#hotspotTextBody");
+        hotspotTextTitle.style.backgroundColor=theData[i].backgroundColor; 
+        hotspotTextTitle.innerHTML=theData[i].textTitle;
+        if (theData[i].textBody && theData[i].textBody.length>0) {
+            hotspotTextBody.innerHTML=theData[i].textBody;
+            hotspotTextBody.style.visibility="visible";
+            }
+            else{
+                hotspotTextBody.style.visibility="hidden";
+            }
+        hotspotTextTitle.style.color=theData[i].textColor;
+        let hotspotHandle=thehotspotComponent.querySelector(".hotspotHandle");
         
         switch (theData[i].direction){
             case "up-left":
-                outlinesPath.setAttribute("d","M50 50 L40 40 L20 40");
-                outlinesCircle.setAttribute("cx",50);
-                hotSpotText.style.left="-205px"
-                hotSpotText.style.top="-40px"
-                labelOutlines.style.left="4px";
+                hotspotTextTitle.style.borderRadius="10px 0 0 0"
+                
             break;
             case "down-right":
-            outlinesPath.setAttribute("d","M53 55 L60 70 L78 70");
-                outlinesCircle.setAttribute("cx",50);
-                hotSpotText.style.left="37px"
-                hotSpotText.style.top="10px"
-                labelOutlines.style.left="4px";
+                hotspotTextTitle.style.borderRadius="0 0 10px 0"
             break;
             case "down-left":
-                outlinesPath.setAttribute("d","M50 50 L40 40 L20 40");
-                outlinesCircle.setAttribute("cx",50);
-                hotSpotText.style.left="-205px"
-                hotSpotText.style.top="-40px"
-                labelOutlines.style.left="4px";
+                hotspotTextTitle.style.borderRadius="0 0 0 10px"
             break;
             case "up-right":
+                hotspotTextTitle.style.borderRadius="0 10px 0 0"
             default:
             break;
         }
-        theHotSpotLabel.style.left=theData[i].x;
-        theHotSpotLabel.style.top=theData[i].y;
-        theHotSpotLabel.style.visibility="visible";
+        thehotspotComponent.style.left=theData[i].x;
+        thehotspotComponent.style.top=theData[i].y;
+        thehotspotComponent.style.visibility="visible";
      }
     
-    data={
-        
-        "hotspots":[
-            {"x":'300px',"y":'50px',"link":"","text":"hello this is the text1", "direction":"up-left","backgroundColor":"#00aabb","textColor":"black"},
-            {"x":"500px","y":'300px',"link":"","text":"hello this is the text2",  "direction":"up-right","backgroundColor":"#ffffff","textColor":"black"},
-            {"x":"300px","y":'200px',"link":"","text":"hello this is the text3",  "direction":"down-right","backgroundColor":"#ffff00","textColor":"white"},
-            {"x":"700px","y":'200px',"link":"","text":"hello this is the text4",  "direction":"down-left","backgroundColor":"#00aabb","textColor":"white"}
-        ]
-    };
+   
     var theData=data.hotspots;
     
     window.onload=()=>{
        
            
         for(let i=0;i<theData.length;i++){       
-            let theClonedNode=theHotSpot.cloneNode(true); 
+            let theClonedNode=thehotspot.cloneNode(true); 
             theClonedNode.setAttribute("number",i);
-            theClonedNode.setAttribute("id","hotSpot"+i);   
+            theClonedNode.setAttribute("id","hotspot"+i);   
             theClonedNode.style.left=theData[i].x;
             theClonedNode.style.top=theData[i].y;
             theClonedNode.addEventListener("click",handleClick)
             theContainer.appendChild(theClonedNode);
         }   
-        theHotSpot.remove(); 
-            
-       
-            
-        
-         
-    }
-    // fetch("./assets/data.json")
-    // .then(response=>response.json())
-    // .then(data=>{
-    //     var theData=data.hotspots;
-    //     for(let i=0;i<theData.length;i++){
-    //         theHotSpot.style.left=theData[i].x;
-    //         theHotSpot.style.top=theData[i].y;
-    //         theHotSpot.innerText+=theData[i].text;
-    //         theContainer.appendChild(theHotSpot);
-    //     }
-    // });
+        thehotspot.remove(); 
+     }
+ 
     
         
     
